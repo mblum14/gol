bbc2bbc61f20d3270a1f1113faf50b0e34f86dd5
Michael Blumberg <mblum14@gmail.com>
2013-10-15 10:35:10 -0400

    Add specs

---
+++ spec/support/game.rb
@@ -0,0 +1,91 @@
+unless defined? Cell
+  require File.join(File.dirname(__FILE__), 'cell')
+end
+
+class Game
+  attr_reader :width, :height, :steps
+
+  def initialize(width, height, seed_probability, steps)
+    @width, @height, @steps = width.to_i, height.to_i, steps.to_i
+    @cells = Array.new(@height) {
+      Array.new(@width) { Cell.new(seed_probability.to_f) } }
+  end
+
+  def self.import_board file, seed_probability=0.1, iterations=100
+    board = file.read
+    verify_board board
+    rows   = board.split(/\r?\n/)
+    height = rows.length
+    width  = rows.first.length
+    game = Game.new(width, height, seed_probability, iterations)
+    game.seed_cells!(board)
+    return game
+  end
+
+  def play!
+    (1..@steps).each do
+      next!
+      system('clear')
+      puts self
+      sleep(0.1)
+    end
+  end
+
+  def next!
+    @cells.each_with_index do |row, y|
+      row.each_with_index do |cell, x|
+        cell.neighbors = alive_neighbours(y, x)
+      end
+    end
+    @cells.each { |row| row.each { |cell| cell.next! } }
+  end
+
+  def alive_neighbours(y, x)
+    [[-1, 0], [1, 0],           # left and right
+     [-1, 1], [0, 1], [1, 1],   # above
+     [-1, -1], [0, -1], [1, -1] # below
+    ].inject(0) do |sum, pos|
+      sum + @cells[(y + pos[0]) % @height][(x + pos[1]) % @width].to_i
+    end # count number of live cells
+  end
+
+  def to_s
+    @cells.map { |row| row.join }.join("\n")
+  end
+
+  def seed_cells!(board)
+    _cells = Array.new(@height) { Array.new(@width) }
+    board.split(/\r?\n/).each_with_index do |row, y|
+      row.split(//).each_with_index do |cell, x|
+        is_alive = cell=='o' ? 1 : 0
+        _cells[y][x] = Cell.new(is_alive)
+      end
+    end
+    @cells = _cells
+  end
+
+  private
+
+  def self.verify_board board
+    rows = board.split(/\r?\n/)
+    # verify rows exist
+    if rows.length.zero?
+      exit_with_error('INVALID BOARD! At least one row must be defined')
+    end
+
+    # verify rows are all the same length
+    unless rows.map(&:length).uniq.count == 1
+      exit_with_error('INVALID BOARD! Rows must all be same length')
+    end
+
+    # verify the characters used are either a space or 'o'
+    if !!board.gsub(/[\\r\\n]+/, "").match(/[^o\s]/)
+      exit_with_error('INVALID BOARD! A cell must be defined using a space or the letter \'o\'') 
+    end
+  end
+
+  def self.exit_with_error message
+    $stderr.puts(message)
+    exit 2
+  end
+end
---
+++ spec/support/game_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+require File.join(File.dirname(__FILE__), '../lib', 'game')
+
+describe Game do
+
+  describe "initialization" do
+    context "directly" do
+      subject { Game.new(5, 4, 1.0, 1) }
+
+      its(:height) { should eql(4) }
+      its(:width)  { should eql(5) }
+      its(:steps)  { should eql(1) }
+    end
+
+    context "from a file" do
+      let(:file) { File.open('spec/support/example_board.txt') }
+      let(:invalid_file_1) { File.open('spec/support/board_without_rows.txt') }
+      let(:invalid_file_2) { File.open('spec/support/board_with_invalid_rows.txt') }
+      let(:invalid_file_3) { File.open('spec/support/board_with_invalid_cells.txt') }
+      subject { Game.import_board(file) }
+
+      context "that is valid" do
+        subject { Game.import_board(file) }
+        its(:height) { should eql(3) }
+        its(:width)  { should eql(3) }
+        its(:steps)  { should eql(100) }
+      end
+
+      context "that specifies no rows" do
+        it "should throw an error" do
+          $stderr.should_receive(:puts).with('INVALID BOARD! At least one row must be defined')
+          lambda { Game.import_board(invalid_file_1); exit }.should raise_error SystemExit
+        end
+      end
+
+      context "that specifies invalid rows" do
+        it "should throw an error" do
+          $stderr.should_receive(:puts).with('INVALID BOARD! Rows must all be same length')
+          lambda { Game.import_board(invalid_file_2); exit }.should raise_error SystemExit
+        end
+      end
+
+      context "that specifies invalid cells" do
+        it "should throw an error" do
+          $stderr.should_receive(:puts).with('INVALID BOARD! A cell must be defined using a space or the letter \'o\'')
+          lambda { Game.import_board(invalid_file_3); exit }.should raise_error SystemExit
+        end
+      end
+    end
+  end
+end
